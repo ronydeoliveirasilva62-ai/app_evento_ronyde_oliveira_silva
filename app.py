4db0981dc903009ce881b5e8e6a7640737060e55
ronydeoliveirasilva62-ai <ronydeoliveirasilva62@gmail.com>
2025-12-11 17:37:34 -0300

    11/12/2025

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/cadastrar-evento")
+def cadastrar_evento():
+    return render_template("cadastra-evento.html")
+
 if __name__ =="__main__":
     app.run(debug=True)
